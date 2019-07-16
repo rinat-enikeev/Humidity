e1714e7bcdbc23d469c7ea5dcbeded2be7280651
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-07-16 16:33:24 +0500

    Initial Commit

---
+++ Humidity/Humidity.h
@@ -0,0 +1,19 @@
+//
+//  Humidity.h
+//  Humidity
+//
+//  Created by Rinat Enikeev on 7/16/19.
+//  Copyright © 2019 Rinat Enikeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Humidity.
+FOUNDATION_EXPORT double HumidityVersionNumber;
+
+//! Project version string for Humidity.
+FOUNDATION_EXPORT const unsigned char HumidityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Humidity/PublicHeader.h>
+
+
