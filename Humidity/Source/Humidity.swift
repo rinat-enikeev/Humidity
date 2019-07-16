4379de05567b62059d1d986e0b1f23d9499b9586
Rinat Enikeev <rinat.enikeev@gmail.com>
2019-07-16 19:09:25 +0500

    add workspace and replace Float80 with Double

--- Humidity/Source/Humidity.swift
+++ Humidity/Source/Humidity.swift
@@ -20,19 +20,19 @@ public struct Humidity {
     
     private let cgkJ = 2.16679 // gk/J
     
-    private let Tc: Float80 = 647.096 // K
-    private let c1: Float80 = -7.85951783
-    private let c2: Float80 = 1.84408259
-    private let c3: Float80 = -11.7866497
-    private let c4: Float80 = 22.6807411
-    private let c5: Float80 = -15.9618719
-    private let c6: Float80 = 1.80122502
-    private let Pc: Float80 = 22064000.0 // Pa
+    private let Tc: Double = 647.096 // K
+    private let c1: Double = -7.85951783
+    private let c2: Double = 1.84408259
+    private let c3: Double = -11.7866497
+    private let c4: Double = 22.6807411
+    private let c5: Double = -15.9618719
+    private let c6: Double = 1.80122502
+    private let Pc: Double = 22064000.0 // Pa
     
-    private let Tn: Float80 = 273.16 // K
-    private let a0: Float80 = -13.928169
-    private let a1: Float80 = 34.707823
-    private let Pn: Float80 = 611.657 // Pa
+    private let Tn: Double = 273.16 // K
+    private let a0: Double = -13.928169
+    private let a1: Double = 34.707823
+    private let Pn: Double = 611.657 // Pa
     
     public init(c: Double, rh: Double) {
         self.c = c
@@ -40,20 +40,18 @@ public struct Humidity {
     }
     
     private func Pws() -> Double {
-        var Pws: Float80
+        var Pws: Double
         if c > 0.01 { // estimate for 0°C-373°C
-            let k = Float80(self.k)
             let n = 1 - (k / Tc)
             let p = Tc / k * (c1 * n + c2 * pow(n, 1.5) + c3 * pow(n, 3) + c4 * pow(n, 3.5) + c5 * pow(n, 4) + c6 * pow(n, 7.5))
-            let l = pow(Float80(M_E), p)
+            let l = pow(M_E, p)
             Pws = Pc * l
         } else { // estimate for -100°C-0.01°C
-            let k = Float80(self.k)
             let n = k / Tn
             let p = a0 * (1 - pow(n, -1.5)) + a1 * (1 - pow(n, -1.25))
-            let l = pow(Float80(M_E), p)
+            let l = pow(M_E, p)
             Pws = Pn * l
         }
-        return Double(Pws)
+        return Pws
     }
 }
